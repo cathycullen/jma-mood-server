044fa9dea58b8eb499286aa17a0d16130a91e0dd
chcculle <cathy@softwareoptions.com>
2014-06-16 14:49:24 -0500

    added setters

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,6 +2,18 @@ get '/' do
   redirect "/profile"
 end
 
+get "/setter" do
+  erb :index
+end
+
+get "/ajax-setter" do
+  session[:foo] = "bar"
+end
+
+get "/results" do
+  erb :results
+end
+
 get '/mood-states' do
 	puts "/mood-states"
 	["Adventurous", "Aggravated"].to_json
