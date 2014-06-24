ce57749a1d18b10a43fa8102da965a95a34303a3
Cathy Cullen <cathy@softwareoptions.com>
2014-06-24 14:06:14 -0500

    fixed merge issues

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,7 +2,6 @@ get '/' do
   redirect "/profile"
 end
 
-<<<<<<< HEAD
 get '/setter' do
 	puts "/setter:  session.inspect: #{session.inspect}"
 	erb :index
@@ -20,23 +19,6 @@ get '/results' do
 	puts "/results session[:foo] : #{session[:foo]}"
 	#puts{session[params[:client_cookie].to _sym]}"
 	session[:foo]
-=======
-get "/setter" do
-  erb :index
-end
-
-get "/ajax-setter" do
-  puts "response.headers: #{response.headers}"
-  session[:foo] = "bar"
-  puts "/ajax-setter session[:foo] #{  session[:foo] }"
-  session[:foo]
-end
-
-get "/results" do
-  puts "/results session[:foo] #{  session[:foo] }"
-  #erb :results
-  session[:foo]
->>>>>>> 6c863debea24c565670fbb46d9ae4a0549ae4a07
 end
 
 get '/mood-states' do
