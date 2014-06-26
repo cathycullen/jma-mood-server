1a4e9bdf51fc92e755150a7f39a748b41755bd75
Cathy Cullen <cathy@softwareoptions.com>
2014-06-26 13:28:30 -0500

    mood-model

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -38,3 +38,33 @@ get "/logged-in" do
 	puts "/logged-in returning: #{retval}"
 	retval
 end
+
+
+get '/submit-mood' do 
+  puts "/submit-mood session.inspect: #{session.inspect}"
+  puts "/submit-mood params #{params}"
+
+  puts "response.headers: #{response.headers}"
+  retval = ""
+	if params[:mood]  && params[:origin]
+    if session[:user_id]
+    	user = User.find(session[:user_id])
+    	if user
+    		mood = Mood.new mood: params[:mood], internal_external: params[:origin]
+    		mood.user = user
+    		mood.save
+    		retval = "#{params[:mood]} submitted}"
+    	else
+    		retval = "user not found "
+    	end
+    else
+    	retval = "session[:user_id] is not defined"
+		end
+	else
+		retval = "missing parameter mood or origin"
+	end
+	puts "/submit-mood returning: #{retval}"
+	retval
+end
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,2 @@
 u1 = User.create(name: "Cathy Cullen", email: "cathy@softwareoptions.com", role:  "admin", password: "123")
 u1 = User.create(name: "Baci", email: "baci@baci.com", role: "client", password: "123", coach: "Jody Michael")
-
