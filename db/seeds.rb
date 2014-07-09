5a56627ffe1215ba89cb59a30444360f21e52a3d
Nate Delage <nate@natedelage.com>
2014-07-09 14:36:12 -0500

    Fix bad merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,14 @@
 
 c = Coach.create(name: "Jody Michael", email: "cathy@softwareoptions.com", password: "baci1313")
-c = Coach.create(name: "Maura Koutoujian", email: "cathy@softwareoptions.com, password: "123")
+c = Coach.create(name: "Maura Koutoujian", email: "cathy@softwareoptions.com", password: "123")
 
 u1 = User.create(name: "Cathy Cullen", email: "cathy@softwareoptions.com", role:  "admin", password: "123", coach_id: 2)
 u2 = User.create(name: "Baci", email: "baci@baci.com", role: "client", password: "123", coach_id: 1)
 
-<<<<<<< HEAD
 m = Mood.create(mood: "Happy", internal_external: "event-driven", thoughts: "holiday weekend", energy_level: 4, user_id: 2)
 m = Mood.create(mood: "Sad", internal_external: "self-directed", thoughts: "Not going to the beach", energy_level: 2, user_id: 2)
-=======
+
 path=APP_ROOT.join('db/seed_data','moods.txt')
->>>>>>> 36fe27097200b48ecc1df4360349fdd225075e3e
 
 path=APP_ROOT.join('db/seed_data','moods.txt')
 File.open(path, "r") do |f|
