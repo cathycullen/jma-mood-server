c3837ecf1ef729420773d925e22a59dddc828cc6
Nate Delage <nate@natedelage.com>
2014-07-19 14:28:39 -0500

    order historical moods

--- app/controllers/moods.rb
+++ app/controllers/moods.rb
@@ -30,7 +30,7 @@ get '/mood-report-last-week' do
   content_type :json
 
   if user_for_auth_token
-    Mood.where(:user_id => user_for_auth_token.id, :created_at => 1.week.ago..Time.now).to_json
+    Mood.where(:user_id => user_for_auth_token.id, :created_at => 1.week.ago..Time.now).order("created_at DESC").to_json
   else
     401
   end
@@ -40,7 +40,7 @@ get '/mood-report-last-month' do
   content_type :json
 
   if user_for_auth_token
-    Mood.where(:user_id => user_for_auth_token.id, :created_at => 1.month.ago..Time.now).to_json
+    Mood.where(:user_id => user_for_auth_token.id, :created_at => 1.month.ago..Time.now).order("created_at DESC").to_json
   else
     401
   end
@@ -49,7 +49,7 @@ end
 get '/send-weekly-mood-report' do
   puts "/send-weekly-mood-report"
   if @user = user_for_auth_token
-      moods = Mood.where(:user_id => @user.id, :created_at => 1.week.ago..Time.now)
+      moods = Mood.where(:user_id => @user.id, :created_at => 1.week.ago..Time.now).order("created_at DESC")
 
       puts "send weekly mood report call mailer"
       email = Mailer.send_weekly_mood_report(@user, @user.coach, moods)
@@ -64,7 +64,7 @@ end
 get '/send-monthly-mood-report' do
   puts "/send-monthly-mood-report"
   if @user = user_for_auth_token
-      moods = Mood.where(:user_id => @user.id, :created_at => 1.month.ago..Time.now)
+      moods = Mood.where(:user_id => @user.id, :created_at => 1.month.ago..Time.now).order("created_at DESC")
 
       email = Mailer.send_weekly_mood_report(@user, @user.coach, moods)
       email.deliver
