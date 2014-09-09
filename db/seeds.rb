388a642342e78e2bac0ab15908aa7684b1433583
Cathy Cullen <cathy@softwareoptions.com>
2014-09-09 10:08:58 -0500

    add timestamps to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
 
+c = Coach.create(name: "Jody Michael", email: "cathy@softwareoptions.com", password: "baci1313")
+c = Coach.create(name: "Maura Koutoujian", email: "cathy@softwareoptions.com", password: "123")
+
+u1 = User.create(name: "Cathy Cullen", email: "cathy@softwareoptions.com", role:  "admin", password: "123", coach_id: 2)
+u2 = User.create(name: "Baci", email: "baci@baci.com", role: "client", password: "123", coach_id: 1)
+
+m = Mood.create(mood: "Happy", internal_external: "event-driven", thoughts: "holiday weekend", energy_level: 4, user_id: 2)
+m = Mood.create(mood: "Sad", internal_external: "self-directed", thoughts: "Not going to the beach", energy_level: 2, user_id: 2)
+
 
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
